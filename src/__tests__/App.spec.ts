de35305b023f67683bbce12936efa5ead93f8731
lbjcena <lbjcena@gmail.com>
2025-12-06 18:07:13 +0100

    fetching test added

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -1,14 +1,15 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 
 import { flushPromises, mount } from '@vue/test-utils'
 import App from '../App.vue'
 import { routes } from '@/router/index'
 import { createMemoryHistory, createRouter } from 'vue-router'
 import { createPinia, setActivePinia } from 'pinia'
+import { getData } from '@/helpers/ApiCaller'
 
 
 describe('App', () => {
-  it('mounts renders properly', async () => {
+  it('App renders and mounts', async () => {
     const pinia = createPinia()
     setActivePinia(pinia)
     const testRouter = createRouter({
@@ -27,5 +28,19 @@ describe('App', () => {
     await testRouter.isReady()
     await flushPromises()
     expect(app.exists()).toBe(true)
+  })
+})
+
+describe('Fetching', () => {
+  global.fetch = vi.fn((): Promise<Response> => Promise.resolve({
+    ok: true,
+    json: async () => Promise.resolve({ name: 'pikachu' })
+  } as Response))
+  it('Fetch code works', async () => {
+    const fetcher = await getData()
+    const getSingle = await fetcher.pokemonSingle(1)
+    const getMulti = await fetcher.pokemonMultiple(1, 1)
+    expect(getSingle).toEqual({ name: 'pikachu' })
+    expect(getMulti).toEqual([{ name: 'pikachu' }])
   })
 })

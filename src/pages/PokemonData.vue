4a4c8b2c8a89b438222accf4f1c2ffcc9ec467ea
lbjcena <lbjcena@gmail.com>
2025-12-13 00:03:19 +0100

    text formatting - added separators between list elements, added target for tsconfig

--- src/pages/PokemonData.vue
+++ src/pages/PokemonData.vue
@@ -6,18 +6,18 @@ import { useRoute } from 'vue-router';
 const route = useRoute()
 const api = getData()
 const pokemon = await api.pokemonSingle(route.params.id as string)
-console.dir(pokemon)
+// console.dir(pokemon)
 
-const regexNoSpecials = /[^a-zA-Z0-9_]+/
+
 </script>
 
 <template>
 
-  <section class="pokemon-page flex flex-col items-center gap-4 text-white">
+  <section class="pokemon-page max-w-[1200px] mx-auto flex flex-col items-center gap-4 text-white">
     <div class="gallery flex">
-      <div v-show="typeof sprite === 'string'" v-for="sprite, key in pokemon.sprites" class="photo-box flex flex-col items-center">
+      <div v-show="typeof sprite === 'string'" v-for="sprite, index in pokemon.sprites" :key="index" class="photo-box flex flex-col items-center">
         <img :src="sprite" alt="" height="200" width="200">
-        <span class="capitalize text-white">{{ String(key).replaceAll('_', ' ') }} </span>
+        <span class="capitalize text-white">{{ String(index).replaceAll('_', ' ') }} </span>
       </div>
     </div>
     <div class="base-data flex flex-col gap-4">
@@ -25,7 +25,7 @@ const regexNoSpecials = /[^a-zA-Z0-9_]+/
       <span>ID: {{ pokemon.id }}</span>
       <span v-for="type in pokemon.types" class="capitalize">Type: {{ type.type.name }}</span>
       <div class="stats flex flex-col gap-2">
-        <h2>Stats</h2>
+        <h2 class="font-semibold text-lg">Stats</h2>
         <table>
           <thead>
             <th v-for="stat in pokemon.stats" class="px-4 py-1 capitalize border-4 border-white">
@@ -39,18 +39,37 @@ const regexNoSpecials = /[^a-zA-Z0-9_]+/
             </th>
           </thead>
           <tbody>
-            <td class="text-center border border-white" v-for="stat in pokemon.stats">{{ stat.base_stat }}</td>
+            <td class="text-center border border-white" v-for="stat, index in pokemon.stats" :key="index">{{ stat.base_stat }}</td>
           </tbody>
         </table>
 
       </div>
-      <ul class="flex flex-col gap-2">
-        <h2>Abilities: </h2>
-        <li class="flex gap-2 capitalize" v-for="ability in pokemon.abilities">
-          {{ ability.ability.name.replace('-', ' ') }}
-        </li>
-      </ul>
+      <div class="flex flex-col gap-2">
+        <h2 class="font-semibold text-lg">Abilities: </h2>
+        <ul class="flex gap-2">
+          <li class="flex gap-2 capitalize" v-for="ability, index in pokemon.abilities" :key="index">
+            {{ ability.ability.name.replace('-', ' ') }}
+          </li>
+        </ul>
+      </div>
+      <div class="flex flex-col gap-4">
+        <h2 class="font-semibold text-lg">Pokemon game appearances: </h2>
+        <ul class="flex max-w-fit flex-wrap">
+          <li class="flex capitalize flex-wrap max-w-fit" v-for="game, index in pokemon.game_indices" :key="index">
+            {{ game.version.name.replace('-', ' ') }}
+          </li>
+
+        </ul>
+      </div>
     </div>
   </section>
 
 </template>
+
+<style scoped>
+/* for cleaner separation of the items on this card */
+ul li:not(:last-child)::after {
+  content: " • ";
+  padding: 0 5px;
+}
+</style>
